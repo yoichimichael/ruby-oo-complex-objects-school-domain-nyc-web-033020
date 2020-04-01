526969554a3b61023255437f1fea1cb075f64add
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-01 08:12:14 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,32 @@
 require 'pry'
 
 class School
-  def initialize(name)
-    @name = name
-        
+  def initialize(school)
+    @school = school
+    @roster = {}
   end
 
   def roster
-    student_hash = {}
-	end
+    @roster
+  end
+
+  def grade(num)
+    @roster[num] 
+  end
+
+  def sort
+    sorted_hash = {}
+    @roster.each { |k,v| sorted_hash[k] = v.sort }
+    sorted_hash
+  end
+  
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
 	
   #def add_student(name, grade)
   #end
