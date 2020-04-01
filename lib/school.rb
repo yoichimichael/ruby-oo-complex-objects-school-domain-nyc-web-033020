5b1806c2f6560860562412a69a4fc259bf02470b
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-03-31 19:47:30 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize(name)
+    @name = name
+        
+  end
+
+  def roster
+    {}
+	end
+	
+  def add_student(name, grade)
+    if roster.has_value?(name)
+      roster[grade] << name
+    else
+      roster[grade] = []
+    end
+  end
+end
+
+school = School.new("Bayside High School")
+school.add_student("AC Slater", 9)
+p school.roster
